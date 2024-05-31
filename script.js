811dedbb486527a5a6181e64b27dfc014dc22bff
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 08:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
-  // your code here
+  // If the string is empty or does not contain any space, return the entire string
+  if (s === '' || s.indexOf(' ') === -1) {
+    return s;
+  }
+  
+  // Split the string by space and return the first element
+  return s.split(' ')[0];
 }
 
 // Do not change the code below
